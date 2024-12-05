b84d34f5d222f9bae3da520bd0898f8ae48eeb51
Hristo101 <92890172+Hristo101@users.noreply.github.com>
2024-12-05 21:27:26 +0200

    HowWeWorkIsReady

---
+++ Dealership/wwwroot/js/faq.js
@@ -0,0 +1,14 @@
+﻿
+document.querySelectorAll('.faq-question').forEach(button => {
+    button.addEventListener('click', () => {
+        const faqItem = button.parentElement;
+
+        document.querySelectorAll('.faq-item').forEach(item => {
+            if (item !== faqItem) {
+                item.classList.remove('active');
+            }
+        });
+
+        faqItem.classList.toggle('active');
+    });
+});
